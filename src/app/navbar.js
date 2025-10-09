d214ab5aaae8a7cdc516796e22ec467b0a8dc42d
Daniel Pomfret <daniel.pomfret1@engineering.digital.dwp.gov.uk>
2025-10-09 21:23:12 +0100

    feat: smaller home button

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
       <nav className="bg-red-100">
         <div>
-          <Link className="px-6 py-8 block w-full text-center text-white bg-gray-900 active:text-violet-500 hover:bg-transparent hover:text-violet-600 focus:outline-none focus:ring text-xl font-bold"
+          <Link className="px-6 py-4 block w-full text-center text-white bg-gray-900 active:text-violet-500 hover:bg-transparent hover:text-violet-600 focus:outline-none focus:ring text-xl font-bold"
             href="/">
             Home
           </Link>
