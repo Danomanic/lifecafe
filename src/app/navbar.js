dff72ea6200a82cca02e67e51f95f847deba50f4
Danomanic <daniel.pomfret@googlemail.com>
2025-12-03 22:43:07 +0000

    feat: navbar switch

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -7,15 +7,14 @@ export default function Navbar() {
       <div>
         <div className="grid grid-cols-3">
           <Modal />
-          <Link className="px-4 py-3 block w-full text-center text-white bg-blue-900 active:text-violet-500 hover:bg-blue-800 focus:outline-none focus:ring text-xl font-bold"
-            href="/">
-            Home
-          </Link>
           <Link className="px-4 py-3 block w-full text-center text-white bg-green-900 active:text-violet-500 hover:bg-green-800 focus:outline-none focus:ring text-xl font-bold"
             href="/orders">
             Orders
           </Link>
-
+          <Link className="px-4 py-3 block w-full text-center text-white bg-blue-900 active:text-violet-500 hover:bg-blue-800 focus:outline-none focus:ring text-xl font-bold"
+            href="/">
+            Home
+          </Link>
         </div>
       </div>
     </nav>
